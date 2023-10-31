098a219602703208038a9854163a9eb6083eb889
Jacob-Ball22 <jeball22@gmail.com>
2023-10-31 14:58:21 -0500

    Updated code and added comments

--- checkbook.py
+++ checkbook.py
@@ -1,18 +1,9 @@
-#imports for reading the directory and text file
+#imports
 import os
 import subprocess
 import datetime
+from checkbook_functions import * 
 
-def get_balance_from_file(filename):
-    with open(filename, 'r') as file:
-        line = file.readline()
-        return float(line.split("$")[1].strip())
-    
-def update_balance_in_file(filename, new_balance):
-    with open(filename, 'w') as file:
-        file.write(f"${new_balance:.2f}")
-
-        
 #starts checkbook if .txt file is present
 if os.path.exists('balance.txt'):
     print('~~~ Welcome to your terminal checkbook! ~~~')
@@ -24,52 +15,39 @@ else:
     print('~~~ Welcome to your terminal checkbook! ~~~')
     
     
-#sets current balance to be retrived by the if statements 
+#sets current balance to be retrived by the if statements and current date
 current_balance = get_balance_from_file('balance.txt')
 now = datetime.datetime.now()
 
 #the main menu that will be repeated until exit is chosen
 loop = True
 while loop == True:
-    
+    #print statements for choices
     print('What would you like to do?\n')
     print('1) view current balance')
     print('2) record a debit (withdraw)')
     print('3) record a credit (deposit)')
     print('4) exit' )
+    #your entry print statement and input
     print('Your Choice?\n')
     choice = input()
 
-
+    #shows current balance through get_balance_from_file function
     if choice == '1':
         print('Your Current Balance Is $' + str(current_balance)+'\n')
-    
+    #uses withdrawmoney function to get withdrawn amount from input
     elif choice == '2':
         print('How Much Would You Like To Withdraw?')
-        withdraw = float(input())
-        if withdraw > current_balance:
-            print('Thats More Than You Own')
-            continue
-        else:
-            new_balance = current_balance - withdraw
-            update_balance_in_file('balance.txt', new_balance)
-            print(f"Your New Balance Is: ${new_balance:.2f}\n")
-            with open('historical_transactions.txt', 'a') as file:
-                file.write(f'\nTransaction: -${withdraw:.2f}\nNew Balance: ${new_balance:.2f}\n' + str(now) +'\n')
-    
+        withdraw = withdraw_money(float(input()), current_balance, now)
+    #uses depositmoney function to get deposited amount from function   
     elif choice == '3':
         print('How Much Would You Like To Deposit?')
-        deposit = float(input())
-        current_balance = get_balance_from_file('balance.txt')
-        new_balance = current_balance + deposit
-        update_balance_in_file('balance.txt', new_balance)
-        print(f"Your New Balance Is: ${new_balance:.2f}\n")
-        with open('historical_transactions.txt', 'a') as file:
-                file.write(f'\nTransaction: ${deposit:.2f}\nNew Balance: ${new_balance:.2f}\n' + str(now) +'\n')
-                
+        deposit = deposit_money(float(input()), current_balance, now)
+    #if choice is equal to 4 then, it sets loop to false so it ends the while statement
     elif choice == '4':
         loop = False
         print('Exiting')
+    #given any non useable inputs it will put out 'invalid choice'   
     else:
         print('Invalid Choice')
     
